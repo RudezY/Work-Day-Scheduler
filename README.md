7c9c03afdb0350f30c5be53ad5acbe0053e9fb73
RudezY <rudyyamashiro@yahoo.com>
2022-10-31 17:55:47 -0700

    added jquery to receive values from local storage to continue to keep main page updated

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # 05 Third-Party APIs: Work Day Scheduler
 
+## I have created a work day scheduler that will save values that are inputted into the text area and once the save button is clicked will save to local storage. Once that is done every 30 seconds the page will update and information that is change as well as depending on the time will vary whether is is defined as past present or future.
+
 ## Your Task
 
 Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
--- index.js
+++ index.js
@@ -2,15 +2,20 @@ $(document).ready(function () {
 var today = moment();
 // This creates the current time and date within the header
 $("#currentDay").text(today.format("[The date today is] dddd MMM Do, YYYY [and the time is] h:mm a"));
+
+$('.saveBtn').on('click', function () {
 // scheduledEvent will grab the value for siblings of the description class (the info that is placed into the text area)
 var scheduledEvent = $(this).siblings(".description").val();
 // time will grab the parent id for example ('#hour-9') 
 var time = $(this).parent().attr('id');
 localStorage.setItem(time, scheduledEvent);
+});
 
+
+// this function updates the class for the timeblock with the proper class description which is either past preset or future. It will receive proper styling when the class is updated.
 function timeUpdate() {
     var currentTime = moment().hours();
-
+// set or removes the class to give the background for each time block the proper color.
     $('.time-block').each(function () {
         var blockHour = parseInt($(this).attr('id').split('-')[1]);
 
@@ -26,5 +31,23 @@ function timeUpdate() {
         }
         });
     }
+    // sets the interval for time Updater to update every 30 seconds
+setInterval(timeUpdater, 30000);
+// here is the function to time updater to run the time update function which updates the time block hours for proper color.
+function timeUpdater(){
     timeUpdate();
+
+};
+// places values saved in local storage and places them in proper id and class
+$('#hour-9 .description').val(localStorage.getItem('hour-9'));
+$('#hour-10 .description').val(localStorage.getItem('hour-10'));
+$('#hour-11 .description').val(localStorage.getItem('hour-11'));
+$('#hour-12 .description').val(localStorage.getItem('hour-12'));
+$('#hour-13 .description').val(localStorage.getItem('hour-13'));
+$('#hour-14 .description').val(localStorage.getItem('hour-14'));
+$('#hour-15 .description').val(localStorage.getItem('hour-15'));
+$('#hour-16 .description').val(localStorage.getItem('hour-16'));
+$('#hour-17 .description').val(localStorage.getItem('hour-17')); 
+
+
 });
